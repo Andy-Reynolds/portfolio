13a4efb62fe8d94968fcd50d8144b1df3b56ed0b
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-04-28 12:19:19 +0100

    Basic positioning of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.scss";
+import useWindowSize from "../../hooks/useWindowSize.js";
+import openMenu from "../../assets/svgs/openMenu.svg";
+import closeMenu from "../../assets/svgs/closeMenu.svg";
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-      <h2 className="navbar__logo">Andy</h2>
-      <div className="navbar__links-container">
-        <ul>
-          <li>
-            <a className="navbar__link" href="/">
-              About
-            </a>
-          </li>
-          <li>
-            <a className="navbar__link" href="/">
-              Project
-            </a>
-          </li>
-          <li>
-            <a className="navbar__link" href="/">
-              Tech Stack
-            </a>
-          </li>
-          <li>
-            <a className="navbar__link" href="/">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
+  const [showMenu, setShowMenu] = useState(false);
+  const windowIsDesktop = useWindowSize(1024);
+  const navbarClass = showMenu ? "navbar navbar--active" : "navbar";
+  const menuIcon = showMenu ? closeMenu : openMenu;
+
+  useEffect(() => {
+    if (windowIsDesktop) {
+      setShowMenu(false);
+    }
+  }, [windowIsDesktop]);
+
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const navMenu = (
+    <div className="navbar__links-container">
+      <ul>
+        <li>
+          <a className="navbar__link" href="/">
+            About
+          </a>
+        </li>
+        <li>
+          <a className="navbar__link" href="/">
+            Project
+          </a>
+        </li>
+        <li>
+          <a className="navbar__link" href="/">
+            Tech Stack
+          </a>
+        </li>
+        <li>
+          <a className="navbar__link" href="/">
+            Contact
+          </a>
+        </li>
+      </ul>
     </div>
+  );
+
+  return (
+    <>
+      <div className={navbarClass}>
+        <h2 className="navbar__logo">Andy</h2>
+        {windowIsDesktop ? (
+          navMenu
+        ) : (
+          <button className="navbar__menu-button" onClick={handleClick}>
+            <img className="navbar__menu-button-image" src={menuIcon} alt="" />
+          </button>
+        )}
+      </div>
+      {!windowIsDesktop && showMenu && navMenu}
+    </>
   );
 };
 
