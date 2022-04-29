4d6be17d5dee1c948ca21f65763e3ca6aeca2be5
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-04-29 11:17:17 +0100

    Added initial data for projects

---
+++ src/assets/projects/projects.js
@@ -0,0 +1,67 @@
+const projects = [
+  {
+    id: 1,
+    title: "Full Stack Project",
+    description: "Project description",
+    stack: ["React", "Java", "GCP"],
+    image: "",
+    github: "https://github.com/nology-tech/positive-pup",
+    preview: "https://nology-tech.github.io/positive-pup/"
+  },
+  {
+    id: 2,
+    title: "Client Project",
+    description: "Project description",
+    stack: ["React"],
+    image: "",
+    github: "https://github.com/Andy-Reynolds/full-stack-project",
+    preview: "https://andy-reynolds.github.io/full-stack-project/"
+  },
+  {
+    id: 3,
+    title: "Punk API",
+    description: "Project description",
+    stack: ["React"],
+    image: "",
+    github: "https://github.com/Andy-Reynolds/punk-api",
+    preview: "https://andy-reynolds.github.io/punk-api/"
+  },
+  {
+    id: 4,
+    title: "Ticket Tracker",
+    description: "Project description",
+    stack: ["React"],
+    image: "",
+    github: "https://github.com/Andy-Reynolds/ticket-tracker",
+    preview: "https://andy-reynolds.github.io/ticket-tracker/"
+  },
+  {
+    id: 5,
+    title: "Morse Code Translator",
+    description: "Project description",
+    stack: ["React"],
+    image: "",
+    github: "https://github.com/Andy-Reynolds/morse-code-translator",
+    preview: "https://andy-reynolds.github.io/morse-code-translator/"
+  },
+  {
+    id: 6,
+    title: "Connect Four",
+    description: "Project description",
+    stack: ["HTML", "CSS", "Javascript"],
+    image: "",
+    github: "https://github.com/Andy-Reynolds/connect-four-project",
+    preview: "https://andy-reynolds.github.io/connect-four-project/"
+  },
+  {
+    id: 7,
+    title: "Calculator",
+    description: "Project description",
+    stack: ["HTML", "CSS", "Javascript"],
+    image: "",
+    github: "https://github.com/Andy-Reynolds/calculator-challenge",
+    preview: "https://github.com/Andy-Reynolds/calculator-challenge"
+  },
+];
+
+export default projects;
