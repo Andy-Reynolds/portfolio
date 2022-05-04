33e78f841d6c57b9691c3a0f46169a528a85fe3b
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-05-04 16:26:50 +0100

    Improved projects section styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,32 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import "./Projects.scss";
 import projects from "../../assets/projects/projects";
+import preview from "../../assets/svgs/preview.svg";
+import chevronLeft from "../../assets/svgs/chevronLeft.svg";
+import chevronRight from "../../assets/svgs/chevronRight.svg";
 
 const Projects = () => {
-  // const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  // const handleIncrement = () => {
-  //   if (counter === projects.length - 1) {
-  //     setCounter(0);
-  //   } else {
-  //     setCounter(counter + 1);
-  //   }
-  // };
+  const handleIncrement = () => {
+    if (counter === projects.length - 1) {
+      setCounter(0);
+    } else {
+      setCounter(counter + 1);
+    }
+  };
 
-  // const handleDecrement = () => {
-  //   if (counter === 0) {
-  //     setCounter(projects.length - 1);
-  //   } else {
-  //     setCounter(counter - 1);
-  //   }
-  // };
+  const handleDecrement = () => {
+    if (counter === 0) {
+      setCounter(projects.length - 1);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
 
-  const stackCards = projects[0].stack.map((tool) => <div>{tool}</div>);
+  const stackCards = projects[counter].stack.map((tool) => (
+    <div className="projects__tool">{tool}</div>
+  ));
 
   console.log(stackCards);
 
@@ -32,25 +37,34 @@ const Projects = () => {
         <div className="projects__tile--upper">
           <div className="projects__image">Image</div>
           <div className="projects__info">
-            <h4 className="projects__title">Project Title</h4>
-            <p className="projects__description">Project Description</p>
-            <div className="projects__stack">Stack {stackCards}</div>
+            <h4 className="projects__title">{projects[counter].title}</h4>
+            <p className="projects__description">
+              {projects[counter].description}
+            </p>
+            <div className="projects__stack-container">{stackCards}</div>
           </div>
         </div>
         <div className="projects__tile--lower">
-          <button className="projects__arrow projects__arrow--left">
-            Left
+          <button
+            className="projects__arrow projects__arrow--left"
+            onClick={handleDecrement}
+          >
+            <img className="arrow-image" src={chevronLeft} alt="left" />
           </button>
           <div className="projects__buttons-container">
             <button className="projects__button project__button--preview">
-              Preview
+              <img className="preview-image" src={preview} alt="preview" />{" "}
+              &ensp; Preview
             </button>
             <button className="projects__button project__button--code">
-              Code
+              &lt;&nbsp;&gt; &nbsp; Code
             </button>
           </div>
-          <button className="projects__arrow projects__arrow--right">
-            Right
+          <button
+            className="projects__arrow projects__arrow--right"
+            onClick={handleIncrement}
+          >
+            <img className="arrow-image" src={chevronRight} alt="right" />
           </button>
         </div>
       </div>
